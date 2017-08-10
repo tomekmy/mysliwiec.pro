66de6f85144d355609fa69857a2cdc00ee3de23e
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2017-08-10 23:44:12 +0200

    Work on contact...

--- js/ang-func.js
+++ js/ang-func.js
@@ -5,7 +5,7 @@ myApp.factory('appServices', function ($timeout) {
     mainLag: function () {
       if ($('#mainText span').length > 115) {
         $('#mainText span:last').remove()
-        var mainTimer = $timeout(function () { }, 5700)
+        var mainTimer = $timeout(function () {}, 5700)
         console.log('Enter from Main. Timeout ID: ' + mainTimer.$$timeoutId)
         return mainTimer
       }
@@ -22,28 +22,44 @@ myApp.config(function ($routeProvider, $locationProvider) {
     .when('/about', {
       templateUrl: 'partials/about.html',
       controller: 'AboutCtrl',
-      resolve: { lag: function (appServices) { return appServices.mainLag() } }
+      resolve: {
+        lag: function (appServices) {
+          return appServices.mainLag()
+        }
+      }
     })
     .when('/portfolio', {
       templateUrl: 'partials/portfolio.html',
       controller: 'PortfolioCtrl',
-      resolve: { lag: function (appServices) { return appServices.mainLag() } }
+      resolve: {
+        lag: function (appServices) {
+          return appServices.mainLag()
+        }
+      }
     })
     .when('/blog', {
       templateUrl: 'partials/blog.html',
       controller: 'BlogCtrl',
-      resolve: { lag: function (appServices) { return appServices.mainLag() } }
+      resolve: {
+        lag: function (appServices) {
+          return appServices.mainLag()
+        }
+      }
     })
     .when('/contact', {
       templateUrl: 'partials/contact.html',
       controller: 'ContactCtrl',
-      resolve: { lag: function (appServices) { return appServices.mainLag() } }
+      resolve: {
+        lag: function (appServices) {
+          return appServices.mainLag()
+        }
+      }
     })
     .otherwise({
       redirectTo: '/main'
     })
 
-    //        $locationProvider.html5Mode(true).hashPrefix('*');
+  //        $locationProvider.html5Mode(true).hashPrefix('*');
 })
 
 myApp.controller('NavCtrl', function ($scope, $location) {
@@ -56,21 +72,49 @@ myApp.controller('NavCtrl', function ($scope, $location) {
   }
 })
 
+// TODO: Fix falling letters scroll issue
 myApp.controller('MainCtrl', function ($scope, $location, $timeout, appServices) {
-  $('#mainText p:eq(0)').show().textillate({
-    in: { effect: 'bounceIn', delay: 40 },
-    out: { effect: 'hinge', delay: 70, shuffle: true, sync: false, delayScale: 7 }
+  $('#mainText p:eq(0)').show().textillate({ in: {
+    effect: 'bounceIn',
+    delay: 40
+  },
+  out: {
+    effect: 'hinge',
+    delay: 70,
+    shuffle: true,
+    sync: false,
+    delayScale: 7
+  }
   })
   var timer1 = $timeout(function () {
-    $('#mainText p:eq(1)').show().textillate({
-      in: { effect: 'bounceIn', delay: 40 },
-      out: { effect: 'hinge', delay: 40, shuffle: true, sync: false, delayScale: 2 }
+    $('#mainText p:eq(1)').show().textillate({ in: {
+      effect: 'bounceIn',
+      delay: 40
+    },
+    out: {
+      effect: 'hinge',
+      delay: 40,
+      shuffle: true,
+      sync: false,
+      delayScale: 2
+    }
     })
   }, 500)
   var timer2 = $timeout(function () {
-    $('#mainText p:eq(2)').show().textillate({
-      in: { effect: 'bounceIn', delay: 40, callback: function () { $('#mainText p:eq(2)').append('<span></span>') } },
-      out: { effect: 'hinge', delay: 40, shuffle: true, sync: false, delayScale: 2 }
+    $('#mainText p:eq(2)').show().textillate({ in: {
+      effect: 'bounceIn',
+      delay: 40,
+      callback: function () {
+        $('#mainText p:eq(2)').append('<span></span>')
+      }
+    },
+    out: {
+      effect: 'hinge',
+      delay: 40,
+      shuffle: true,
+      sync: false,
+      delayScale: 2
+    }
     })
   }, 3100)
 
