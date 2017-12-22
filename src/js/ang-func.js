7f0d9ed5e1b1089a3b82aefa25e4cdcf23a604f3
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2017-12-22 22:58:40 +0100

    Remove unnecessary comment

--- src/js/ang-func.js
+++ src/js/ang-func.js
@@ -49,7 +49,6 @@ myApp.service('appServices', ['$timeout', '$location', '$window', function ($tim
     },
     // Email validation in contact form
     validateEmail: function (email) {
-      // var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       var re = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
     }
